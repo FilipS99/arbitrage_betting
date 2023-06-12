818076cfd8cb6e474459acfb72af1e664bf4634c
FilipS99 <fstolarczyk99@gmail.com>
2023-06-12 21:39:17 +0200

    +datetime

--- scrape_forbet.py
+++ scrape_forbet.py
@@ -1,14 +1,10 @@
-import traceback
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.action_chains import ActionChains
 import numpy as np
 import pandas as pd
 import time
+from datetime import datetime, date
 
 from additional_functions import scroll_into_view
 
@@ -33,7 +29,7 @@ def scrape_forbet() -> pd.DataFrame():
     options.add_argument("--start-maximized")
     options.add_argument('--ignore-certificate-errors')
     driver = webdriver.Chrome(options=options)
-    driver.implicitly_wait(5)
+    driver.implicitly_wait(3)
            
     for link in links:
         # unpack tuple
@@ -44,52 +40,65 @@ def scrape_forbet() -> pd.DataFrame():
         
         # in case of 'stale' elements
         time.sleep(3)
+        
+        # scrape sections 
+        sections = driver.find_elements(By.XPATH,'/html/body/div[1]/div/div/main/div[2]/div/div/div/div[1]/section/div/section[*]/div/section') 
 
-        # get elements
-        elements = driver.find_elements(By.XPATH, '/html/body/div[1]/div/div/main/div[2]/div/div/div/div[1]/section/div/section[*]/div/section[*]/div[*]') 
-                                                                
-        for element in elements:
-            scroll_into_view(driver, element, sleep=0.1)
-            
-            item = element.text.split('\n')[1:]
-            teams = item[0].split(' - ')
+        for section in sections:
+            scroll_into_view(driver, section, sleep=0)
 
-            # remove random value
-            while 'BETARCHITEKT' in item:
-                item.remove('BETARCHITEKT')
-            
-            if bet_outcomes == 'two-way':
-                # if invalid data - skip 
-                if len(item) < 4 or len(teams) != 2 or not item[1].replace(".", "").isnumeric() or not item[2].replace(".", "").isnumeric():
-                    print("ForBet: Error appending - " + " | ".join(item))
-                    continue
+            # set event date 
+            events_date = section.text.split('\n')[0].split(', ')[1].replace('.', '-') + '-' + str(date.today().year)
 
-                # append item
-                dct = {"team_1": teams[0],
-                    "team_2": teams[1],
-                    "stake_1_wins": item[1],
-                    "stake_draw": np.inf,
-                    "stake_2_wins": item[2],
-                    "url": url,
-                    "category": category}
-            
-            elif bet_outcomes == 'three-way':
-                # if invalid data - skip 
-                if len(item) < 5 or len(teams) != 2 or not item[1].replace(".", "").isnumeric() or not item[2].replace(".", "").isnumeric() or not item[3].replace(".", "").isnumeric():
-                    print("ForBet: Error appending - " + " | ".join(item))
-                    continue
+            # get elements
+            elements = section.find_elements(By.XPATH, './div[*]') 
+                                                                    
+            for element in elements:
+                scroll_into_view(driver, element, sleep=0.1)
+                
+                item = element.text.split('\n')[1:]
+                teams = item[0].split(' - ')
 
-                # append item
-                dct = {"team_1": teams[0],
-                    "team_2": teams[1],
-                    "stake_1_wins": item[1],
-                    "stake_draw": item[2],
-                    "stake_2_wins": item[3],
-                    "url": url,
-                    "category": category}\
-                    
-            df = df._append(pd.DataFrame(
-                [dct], columns=columns), ignore_index=True)
+                # remove random value
+                while 'BETARCHITEKT' in item:
+                    item.remove('BETARCHITEKT')
+                
+                if bet_outcomes == 'two-way':
+                    # if invalid data - skip 
+                    if len(item) < 4 or len(teams) != 2 or not item[1].replace(".", "").isnumeric() or not item[2].replace(".", "").isnumeric():
+                        print("ForBet: Error appending - " + " | ".join(item))
+                        continue
+
+                    # append item
+                    dct = {"game_datetime": events_date + ' ' + item[0],
+                           "team_1": teams[0],
+                           "team_2": teams[1],
+                           "stake_1_wins": item[1],
+                           "stake_draw": np.inf,
+                           "stake_2_wins": item[2],
+                           "url": url,
+                           "category": category
+                        }
+                
+                elif bet_outcomes == 'three-way':
+                    # if invalid data - skip 
+                    if len(item) < 5 or len(teams) != 2 or not item[1].replace(".", "").isnumeric() or not item[2].replace(".", "").isnumeric() or not item[3].replace(".", "").isnumeric():
+                        print("ForBet: Error appending - " + " | ".join(item))
+                        continue
+
+                    # append item
+                    dct = {"game_datetime": events_date + ' ' + item[0],
+                           "team_1": teams[0],
+                           "team_2": teams[1],
+                           "stake_1_wins": item[1],
+                           "stake_draw": item[2],
+                           "stake_2_wins": item[3],
+                           "url": url,
+                           "category": category
+                        }
+                        
+                df = df._append(pd.DataFrame(
+                    [dct], columns=columns), ignore_index=True)
 
     # close chrome
     driver.quit()
@@ -103,5 +112,5 @@ def scrape_forbet() -> pd.DataFrame():
 
 # df = pd.DataFrame()
 # df = df._append(scrape_forbet(), ignore_index=True)
-# print(df.head())
+print(df.head())
 
